7a4ea4cf5d99a9d0929719c891291c3de3dcc7b8
lolu1023 <soowkw@naver.com>
2022-08-07 21:27:16 +0900

    라이프사이클

---
+++ 5.가위바위보/RSP.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Component } from 'react'
+
+// 라이프사이클
+// 클래스의 경우 -> constructor -> render -> ref -> componentDidMount
+// -> (setState/props 바뀔때 -> shouldComponentUpdate -> render -> componentDidUpdate)
+// 부모가 나를 없앴을 때 -> componentWillUnmount -> 소멸
+
+const rspCoords = {
+  바위: '0',
+  가위: '-142px',
+  보: '-284px',
+}
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1,
+}
+
+class RSP extends Component {
+  state = {
+    result: '',
+    imgCoord: '0',
+    score: 0,
+  };
+
+  interval;
+
+  componentDidMount() { // 컴포넌트가 첫 렌더링된 후 , 여기에 비동기 요청을 많이 한다
+    this.interval = setInterval(() => {
+      const { imgCoord } = this.state; // -142px
+      console.log('hello', this.state.imgCoord, rspCoords.가위)
+      if(imgCoord === rspCoords.바위) {
+        this.setState({
+          imgCoord: rspCoords.가위,
+        })
+      } else if(imgCoord === rspCoords.가위) {
+        this.setState({
+          imgCoord: rspCoords.보,
+        })
+      }else if(imgCoord === rspCoords.보) {
+        this.setState({
+          imgCoord: rspCoords.바위,
+        })
+      }
+    }, 1000)
+  }
+
+  componentDidCatch() { // 리렌더링
+
+  }
+  componentWillUnmount() { // 컴포넌트가 제거되기 직전, 비동기 요청 정리를 많이 한다
+    clearInterval(this.interval)
+  }
+
+  onClickBtn = (choice) => {
+    
+  }
+
+  render() {
+    const { result, score, imgCoord } = this.state;
+    return (
+      <>
+        <div id="computer" style={{backgroundImage: 'url(https://en.pimg.jp/023/182/267/1/23182267.jpg)', backgroundPosition:`${imgCoord} 0`}} />
+        <div>
+          <button id="rock" className="btn" onClick={this.onClickBtn('바위')}>바위</button>
+          <button id="scissor" className="btn" onClick={this.onClickBtn('가위')}>가위</button>
+          <button id="paper" className="btn" onClick={this.onClickBtn('보')}>보</button>
+        </div>
+        <div>{result}</div>
+        <div>현재 {score}점</div>
+      </>
+    );
+  }
+}
+
+export default RSP
